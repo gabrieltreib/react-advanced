fd3d6120f022127bc2936870a4326646dc73787c
Gabriel Treib <treib159@gmail.com>
2021-07-25 13:47:17 -0300

    fixing restore "items button"

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,28 +3,34 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
-  const removeItem = (id) => {
+
+  //criando a constante people 2 e adicionando o conteúdo de 'data' para dentro dela
+  //usando o spread operator.
+  //após isso, apenas passado a variável people2 para dentro do método setPeople
+  //chamado no click do botão 'restore items'
+  const people2 = [];
+  people2.push(...data);
+
+  const removePeople = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
 
-  const restoreItems = () => { };
   return (
-    //<> = react fragment
     <>
+
       {people.map((person) => {
         //desestruturando o array person em 2 variáveis
         const { id, name } = person;
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}> remove </button>
+            <button onClick={() => removePeople(id)}> remove </button>
           </div>
         );
       })}
       <button className='btn' onClick={() => setPeople([])} > clear items </button>
-      <button className='btn' onClick={() => restoreItems()}> restore items </button>
-
+      <button className='btn' onClick={() => setPeople(people2)}> restore items </button>
     </>
   );
 };
