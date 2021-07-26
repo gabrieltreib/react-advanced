c0d809950daea1613735c9331d3258c7f77db291
gabrieltreib <treib159@gmail.com>
2021-07-26 06:18:12 -0300

    alteracoes do dia 26/07/2021

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,24 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (value >= 1) {
+      document.title = `new message (${value})`;
+    } else {
+
+    }
+    //se você quer que o useEffect execute apenas uma vez, colocar o array vazio
+    //como segundo parâmetro
+  }, [value]);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => { setValue(value + 1) }}> click me </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
