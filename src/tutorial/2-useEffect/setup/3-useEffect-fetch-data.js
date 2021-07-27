dbea15b80ee177e6399d48af01db3f9c7d17bf73
gabrieltreib <treib159@gmail.com>
2021-07-27 06:41:19 -0300

    alteracoes do dia 27/07/2021

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,6 +5,7 @@ const url = 'https://api.github.com/users';
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
+  //testando wakatime
   const getUsers = async () => {
     const response = await fetch(url);
     const users = await response.json();
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,24 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  return (<>
+    <h1>{text || 'gabriel treib'}</h1>
+    <button className='btn' onClick={() => setIsError(!isError)}> toggle error </button>
+    {isError && <h1>Error...</h1>}
+    {isError ? (
+      <p>there is an error...</p>
+    ) : (
+      <div>
+        <h2>there is no error</h2>
+      </div>
+    )}
 
-  return <h2>short circuit</h2>;
+  </>);
 };
 
 export default ShortCircuit;
